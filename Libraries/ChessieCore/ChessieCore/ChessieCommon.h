21797023ed29542b48e1d98c23e03e89f72f8c92
Dogukan Arat <aratdogukan@gmail.com>
2022-02-06 19:42:16 +0300

    Rename generic ctype

--- Libraries/ChessieCore/ChessieCore/ChessieCommon.h
+++ Libraries/ChessieCore/ChessieCore/ChessieCommon.h
@@ -8,7 +8,7 @@ typedef uint16_t UInt16;
 typedef uint32_t UInt32;
 typedef uint64_t UInt64;
 typedef int8_t Int8;
-typedef int16_t CInt16;
+typedef int16_t Int16;
 typedef int32_t Int32;
 typedef int64_t Int64;
 typedef float Real32;
