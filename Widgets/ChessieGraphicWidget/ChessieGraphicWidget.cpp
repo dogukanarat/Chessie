e5b844dd3563bc516031f0d0869b77d265d1ecff
Dogukan Arat <aratdogukan@gmail.com>
2022-02-27 14:23:39 +0300

    Add simulation methods and test

--- Widgets/ChessieGraphicWidget/ChessieGraphicWidget.cpp
+++ Widgets/ChessieGraphicWidget/ChessieGraphicWidget.cpp
@@ -4,10 +4,50 @@
 ChessieGraphicWidget::ChessieGraphicWidget(QWidget *parent)
     : ChessieBaseWidget(parent)
     , m_ui(new Ui::ChessieGraphicWidget)
+    , m_scene{}
+    , m_timer{}
 {
     m_ui->setupUi(this);
+
+    fps = 60;
+
+    m_timer.setInterval(QVariant(1000 / fps).toInt());
+    m_timer.setSingleShot(false);
+    m_timer.start();
+
+    initialise();
+
+    connect(&m_timer, &QTimer::timeout, this, &ChessieGraphicWidget::update);
 }
 
 ChessieGraphicWidget::~ChessieGraphicWidget()
 {
+}
+
+void ChessieGraphicWidget::initialise()
+{
+    length = 100;
+    dps = 50;
+    phi = 0;
+
+    x = length * qCos(phi * M_PI / 180);
+    y = length * qSin(phi * M_PI / 180);
+
+    m_line = m_scene.addLine(0, 0, x, y);
+    m_ellipse = m_scene.addEllipse(x, y, 10, 10);
+
+    m_scene.setSceneRect(-180, -90, 360, 180);
+    m_ui->graphicsView->setScene(&m_scene);
+    m_ui->graphicsView->show();
+}
+
+void ChessieGraphicWidget::update()
+{
+    t++;
+    phi = QVariant( t * ( dps / fps ) ).toInt() % 360;
+    x = length * qCos(phi * M_PI / 180);
+    y = length * qSin(phi * M_PI / 180);
+
+    m_line->setLine(0, 0, x, y);
+    m_ellipse->setRect(x - 5, y - 5 , 10, 10);
 }
--- Widgets/ChessieGraphicWidget/ChessieGraphicWidget/ChessieGraphicWidget.h
+++ Widgets/ChessieGraphicWidget/ChessieGraphicWidget/ChessieGraphicWidget.h
@@ -1,4 +1,8 @@
 #include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsLineItem>
+#include <QTimer>
+#include <QtMath>
 #include "ChessieBaseWidget/ChessieBaseWidget.h"
 
 namespace Ui
@@ -14,6 +18,22 @@ public:
     ChessieGraphicWidget(QWidget *parent = 0);
     ~ChessieGraphicWidget();
 
+public slots:
+    void initialise();
+    void update();
+
 private:
     Ui::ChessieGraphicWidget* m_ui;
+    QGraphicsScene m_scene;
+    QTimer m_timer;
+    QGraphicsLineItem* m_line;
+    QGraphicsEllipseItem* m_ellipse;
+
+    UInt32 fps;
+    Real32 dps;
+    UInt32 t;
+    Real32 phi;
+    Real32 length;
+    Real32 x;
+    Real32 y;
 };
